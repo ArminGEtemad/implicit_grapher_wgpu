3af4f917051b4396faad93a0a6af9e6b7cfe6061
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-02-26 22:48:58 +0100

    Added render pass
    State is now connected to the gpu and can communicate

--- grapher3d/src/state.rs
+++ grapher3d/src/state.rs
@@ -2,17 +2,22 @@ use std::sync::Arc;
 use wgpu::SurfaceError;
 use winit::{dpi::PhysicalSize, window::Window};
 
-use crate::gpu_resource::{FrameContext, GpuResource};
+use crate::{
+    gpu_connector::GpuConnector,
+    gpu_resource::{FrameContext, GpuResource},
+};
 
 pub struct State {
     gpu_res: GpuResource,
+    connector: GpuConnector,
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> Result<Self, String> {
         let gpu_res = GpuResource::new(window).await?;
+        let connector = GpuConnector::new(&gpu_res);
 
-        Ok(Self { gpu_res })
+        Ok(Self { gpu_res, connector })
     }
 
     pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
@@ -20,7 +25,8 @@ impl State {
     }
 
     pub fn render(&mut self) -> Result<(), SurfaceError> {
-        let frame: FrameContext = self.gpu_res.begin_frame()?;
+        let mut frame: FrameContext = self.gpu_res.begin_frame()?;
+        self.connector.render_pass(&mut frame);
         self.gpu_res.submit_frame(frame);
         Ok(())
     }
