027ce29c3ef981c8107980a5303b32a9f5bc8063
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-03-18 01:02:47 +0100

    Added shader watcher

--- grapher3d/src/state.rs
+++ grapher3d/src/state.rs
@@ -5,11 +5,13 @@ use winit::{dpi::PhysicalSize, window::Window};
 use crate::{
     gpu_connector::GpuConnector,
     gpu_resource::{FrameContext, GpuResource},
+    shader_watcher::ShaderWatcher,
 };
 
 pub struct State {
     gpu_res: GpuResource,
     connector: GpuConnector,
+    shader_watcher: ShaderWatcher,
 }
 
 impl State {
@@ -17,7 +19,15 @@ impl State {
         let gpu_res = GpuResource::new(window).await?;
         let connector = GpuConnector::new(&gpu_res);
 
-        Ok(Self { gpu_res, connector })
+        let shader_path = format!("{}/shaders", env!("CARGO_MANIFEST_DIR"));
+        println!("Watching shaders at: {}", shader_path);
+        let shader_watcher = ShaderWatcher::new(shader_path);
+
+        Ok(Self {
+            gpu_res,
+            connector,
+            shader_watcher,
+        })
     }
 
     pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
@@ -25,6 +35,11 @@ impl State {
     }
 
     pub fn render(&mut self) -> Result<(), SurfaceError> {
+        while let Ok(path) = self.shader_watcher.reciever_x.try_recv() {
+            println!("Shader changed: {:?}", path);
+            self.connector.rebuild_pipeline(&self.gpu_res);
+        }
+
         let mut frame: FrameContext = self.gpu_res.begin_frame()?;
         self.connector.render_pass(&mut frame);
         self.gpu_res.submit_frame(frame);
