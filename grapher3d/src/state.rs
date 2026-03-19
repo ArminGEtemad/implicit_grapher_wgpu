61eea1b8561494c872cb611a28d9aa7dcc93c87c
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-03-19 14:06:27 +0100

    Added keyboard and mouse input to control camera
    Now the position of the camera can be manipulated using WASD and mouse wheel. Using the WASD we can rotate and mouse wheel allows us to zoom in and out. Right now we always look at (0, 0, 0)

--- grapher3d/src/state.rs
+++ grapher3d/src/state.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use wgpu::SurfaceError;
-use winit::{dpi::PhysicalSize, window::Window};
+use winit::{dpi::PhysicalSize, event::MouseScrollDelta, keyboard::KeyCode, window::Window};
 
 use crate::{
     gpu_connector::GpuConnector,
@@ -11,6 +11,7 @@ use crate::{
 pub struct State {
     gpu_res: GpuResource,
     connector: GpuConnector,
+    camera_spherical_coord: [f32; 3], // r, thera, phi
     shader_watcher: ShaderWatcher,
 }
 
@@ -21,13 +22,65 @@ impl State {
 
         let shader_path = format!("{}/shaders", env!("CARGO_MANIFEST_DIR"));
         println!("Watching shaders at: {}", shader_path);
+
+        // r theta phi
+        let camera_spherical_coord = [3.0, 0.1, 0.1];
+
         let shader_watcher = ShaderWatcher::new(shader_path);
 
         Ok(Self {
             gpu_res,
             connector,
+            camera_spherical_coord,
             shader_watcher,
         })
+    }
+
+    fn converter_coord_for_gpu(&self) {
+        let r = self.camera_spherical_coord[0];
+        let theta = self.camera_spherical_coord[1];
+        let phi = self.camera_spherical_coord[2];
+
+        // (r, theta, phi) -> (x, y, z)
+        // x = r * sin(theta) * cos(phi)
+        // y = r * sin(theta) * sin(phi)
+        // z = r * cos(theta)
+
+        let x = r * phi.sin() * theta.cos();
+        let y = r * phi.cos();
+        let z = r * phi.sin() * theta.sin();
+
+        self.connector.update_camera_pos(&self.gpu_res, [x, y, z]);
+    }
+
+    pub fn update_camera_input_keyboard(&mut self, key: KeyCode) {
+        let sensitivity = 0.05;
+
+        match key {
+            KeyCode::KeyA => self.camera_spherical_coord[1] += sensitivity,
+            KeyCode::KeyD => self.camera_spherical_coord[1] -= sensitivity,
+            KeyCode::KeyW => {
+                self.camera_spherical_coord[2] =
+                    (self.camera_spherical_coord[2] - sensitivity).clamp(0.01, 3.1) // needed against gimbal lock
+            }
+            KeyCode::KeyS => {
+                self.camera_spherical_coord[2] =
+                    (self.camera_spherical_coord[2] + sensitivity).clamp(0.01, 3.1) // needed against gimbal lock
+            }
+            _ => return,
+        }
+
+        self.converter_coord_for_gpu();
+    }
+
+    pub fn update_camera_input_mouse(&mut self, delta: MouseScrollDelta) {
+        let scroll_d = match delta {
+            MouseScrollDelta::LineDelta(_, y) => y * -0.05,
+            MouseScrollDelta::PixelDelta(pos) => pos.y as f32 * -0.05,
+        };
+
+        self.camera_spherical_coord[0] += scroll_d;
+        self.converter_coord_for_gpu();
     }
 
     pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
